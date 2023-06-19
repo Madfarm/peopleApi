1a766fabde2b68dd5233b00752fa011690b19fe3
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-19 01:02:06 -0400

    Add people controller and index route

--- Models/People.cs
+++ Models/People.cs
@@ -3,6 +3,7 @@ namespace PeopleApi.Models;
 
 public record Person
 {
+    public Guid Id { get; init; }
     public string Name { get; init; }
     public int Age { get; init; }
     public string Pic { get; init; }
