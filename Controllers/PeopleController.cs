9813d216bfb82a589b2f83c22f364850fe1033eb
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-25 18:45:43 -0400

    Scaffold the mongo settings

--- Controllers/PeopleController.cs
+++ Controllers/PeopleController.cs
@@ -7,16 +7,9 @@ namespace PeopleApi.Controllers;
 [Route("[controller]")]
 public class PeopleController :  ControllerBase
 {
-    [HttpGet]
-    public IEnumerable<Person> GetPeople()
-    {
-        List<Person> people = new()
-        {
-            new Person { Id= new Guid() , Name = "Matt", Age = 6, Pic = "https://placekitten.com/200/300" },
-            new Person { Id= new Guid() , Name = "Hiomno", Age = 57, Pic = "https://placekitten.com/200/300" },
-            new Person { Id= new Guid() , Name = "Cry-sis", Age = 21, Pic = "https://placekitten.com/200/300" },
-            new Person { Id= new Guid() , Name = "Simpman", Age = 35, Pic = "https://placekitten.com/300/200" }
-        };
-        return people;
-    }
+    // [HttpGet]
+    // public IEnumerable<Person> GetPeople()
+    // {
+        
+    // }
 }
