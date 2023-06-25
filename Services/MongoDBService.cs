00c6f6d7e54830b33e3638a73ff0a9f6f9adb04e
AnthonyScott <anthony.scott1005@gmail.com>
2023-06-25 19:18:59 -0400

    Scaffold create service

--- Services/MongoDBService.cs
+++ Services/MongoDBService.cs
@@ -2,10 +2,11 @@ using Microsoft.Extensions.Options;
 using MongoDB.Driver;
 using PeopleApi.Database;
 using PeopleApi.Models;
+using PeopleApi.Repositories;
 
 namespace PeopleApi.Services;
 
-public class MongoDBService
+public class MongoDBService : IPeopleRepository
 {
     private readonly IMongoCollection<Person> _peopleCollection;
     private readonly FilterDefinitionBuilder<Person> filterBuilder = Builders<Person>.Filter;
@@ -17,5 +18,31 @@ public class MongoDBService
         _peopleCollection = database.GetCollection<Person>(mongoDBSettings.Value.CollectionName);
 
         System.Console.WriteLine(_peopleCollection);
+    }
+
+    public async Task CreatePerson(Person person)
+    {
+        await _peopleCollection.InsertOneAsync(person);
+        return;
+    }
+
+    public Task<DeleteResult> DeletePerson()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Person> GetOnePerson()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<Person>> GetPeople()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ReplaceOneResult> UpdatePerson()
+    {
+        throw new NotImplementedException();
     }
 }
