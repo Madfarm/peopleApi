bc48e13993db21ce9083b085e083512f42e6a3ef
AnthonyScott <anthony.scott1005@gmail.com>
2023-07-01 20:34:08 -0400

    configure health checks and health check endpoint

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
@@ -25,5 +26,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
